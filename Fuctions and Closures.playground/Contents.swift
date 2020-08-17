24e5a758bf4bc6529ff0682adfbeff56d7961dc4
saochan-lab <68805078+saochan-lab@users.noreply.github.com>
2020-08-18 00:50:41 +0900

    Function and Closuresの一部を学んだ

--- Fuctions and Closures.playground/Contents.swift
+++ Fuctions and Closures.playground/Contents.swift
@@ -1,3 +1,28 @@
-import UIKit
+func greet(person: String, lunch: String) -> String {
+    return "Hello \(person), today's lunch special is \(lunch)."
+}
+greet(person: "Bob", lunch: "ramen")
 
-var str = "Hello, playground"
+func greet(_ person: String, on day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+greet("John", on: "Webnesday")
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+    return (min, max, sum)
+}
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+print(statistics.2)
