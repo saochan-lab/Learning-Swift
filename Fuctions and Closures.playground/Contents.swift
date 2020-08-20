4579a00c9a3c337d9695a792146ff7a0bbfc8724
saochan-lab <68805078+saochan-lab@users.noreply.github.com>
2020-08-20 23:10:24 +0900

    Functions and Closuresを一部学んだ

--- Fuctions and Closures.playground/Contents.swift
+++ Fuctions and Closures.playground/Contents.swift
@@ -46,3 +46,16 @@ func makeIncrementer() -> ((Int) -> Int) {
 var increment = makeIncrementer()
 increment(7)
 
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+var numbers = [20, 19, 17, 12]
+hasAnyMatches(list: numbers, condition: lessThanTen)
