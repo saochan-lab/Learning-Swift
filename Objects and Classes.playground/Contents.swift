44f74333b2900831863364ac86ba985527dc515b
saochan-lab <68805078+saochan-lab@users.noreply.github.com>
2020-08-22 00:11:46 +0900

    Objects and Classesを勉強し始めた

---
+++ Objects and Classes.playground/Contents.swift
@@ -0,0 +1,12 @@
+class shape {
+    var numberOfSides = 0
+    let A = "Hello"
+    func B(a: Int) -> String{
+        print(a)
+        return "\(A)"
+    }
+    func simpleDiclaration() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
