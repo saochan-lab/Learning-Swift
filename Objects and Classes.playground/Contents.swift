a3a6594c882ac767d1648efcd0b5b8ac988c0b59
saochan-lab <68805078+saochan-lab@users.noreply.github.com>
2020-08-25 22:32:10 +0900

    Object and Classes を一部学んだ

--- Objects and Classes.playground/Contents.swift
+++ Objects and Classes.playground/Contents.swift
@@ -41,3 +41,30 @@ class NamedShape {
 
 var namedShape = NamedShape(name: "Triangle")
 namedShape.name
+
+//class B: NamedShape { }
+//
+//let b = B (name: <#T##String#>)
+
+
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDiclaration() -> String {
+        return "A shape with side of length \(sideLength)"
+    }
+}
+
+let test = Square(sideLength: 5.2, name: "my test length")
+test.area()
+test.simpleDiclaration()
