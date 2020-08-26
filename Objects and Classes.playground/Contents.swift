f1861c494af5a0e94245b4881720cd98b718801c
saochan-lab <68805078+saochan-lab@users.noreply.github.com>
2020-08-26 23:47:43 +0900

    Object and Classesを一部学んだ

--- Objects and Classes.playground/Contents.swift
+++ Objects and Classes.playground/Contents.swift
@@ -91,3 +91,31 @@ class Circle: NamedShape {
 let test2 = Circle(radias: 2.4, name: "my test radias")
 test2.area()
 test2.simpleDiclaration()
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDiclaration() -> String {
+        return "A equilateral triangle with sides of length \(sideLength)"
+    }
+}
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.perimeter)
